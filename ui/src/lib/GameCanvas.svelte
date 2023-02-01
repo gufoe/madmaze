fb4da126dec9fbfd21149de6bc3ebb8c23550faf
Giacomo Rizzi <gufoes@gmail.com>
2023-02-01 21:48:55 +0100

    map scaling, all features

--- ui/src/lib/GameCanvas.svelte
+++ ui/src/lib/GameCanvas.svelte
@@ -1,92 +1,139 @@
 <script lang="ts">
   import { onMount } from "svelte";
-  import { Game, MAPS } from "../../lib/game";
+  import { boundingBox, Game, getRatio, MAPS } from "../../lib/game";
   import Circle from "./Graphics/Circle.svelte";
   import Rect from "./Graphics/Rect.svelte";
+  import Text from "./Graphics/Text.svelte";
   let canvas: Element;
   let game: Game = new Game(MAPS.DEFAULT);
-  let tick = 0;
-  let death_elapesed = false;
+  let scale = 1;
+  let map_size;
+  onMount(() => {
+    scale = getRatio(
+      {
+        w: canvas.clientWidth,
+        h: canvas.clientHeight,
+      },
+      game.map.tiles
+    );
+    map_size = boundingBox(game.map.tiles);
+  });
+  let show_restart = false;
   let int: NodeJS.Timeout;
   setInterval(() => {
-    tick++;
     game = game.update();
     if (game.pl.status == "dead" && !int) {
       int = setTimeout(() => {
-        death_elapesed = true;
-      }, 1000);
+        int = undefined;
+        alert("Che scarso!");
+        game.initGame();
+        // show_restart = true;
+      }, 100);
+    }
+    if (game.pl.status == "victory" && !int) {
+      int = setTimeout(() => {
+        int = undefined;
+        alert(
+          "Hai vinto!\n" +
+            ((game.pl.end - game.pl.start) / 1000).toFixed(2) +
+            " secondi"
+        );
+        game.initGame();
+      }, 100);
     }
   }, 1000 / 30);
   const handleRestart = () => {
-    death_elapesed = false;
+    show_restart = false;
     int = undefined;
     game.initGame();
   };
 </script>
 
-<div bind:this={canvas} class="relative flex flex-col flex-grow">
-  <div class="relative flex flex-grow bg-black overflow-hidden">
-    <div style="scale:1" class="absolute bg-green">
-      {#each game.squares as square}
-        <Rect
+<div class="relative flex flex-col flex-grow">
+  <div bind:this={canvas} class="flex flex-col grow">
+    <div
+      class="relative flex overflow-hidden m-auto"
+      style="width:{map_size?.w * scale}px; height:{map_size?.h * scale}px"
+    >
+      <div style="scale:{scale}" class="absolute bg-green">
+        {#each game.map.tiles as rect}
+          <Rect
+            shape={Object.assign(
+              {
+                color: rect.victory ? "#0ff" : "#f00",
+              },
+              rect
+            )}
+          />
+        {/each}
+        {#if game.pl.start}
+          <Text
+            shape={{
+              x: game.map.pl.x,
+              y: game.map.pl.y,
+              align: "center",
+              color: "white",
+              w: 50,
+              text:
+                (
+                  ((game.pl.end ?? new Date().getTime()) - game.pl.start) /
+                  1000
+                ).toFixed(2) + "s",
+            }}
+          />
+        {/if}
+        <Circle
           shape={Object.assign(
             {
-              color: square.victory ? "#0ff" : "#f00",
+              color: {
+                idle: "#ff0",
+                alive: "#0f0",
+                dead: "#f00",
+                victory: "#fff",
+              }[game.pl.status],
             },
-            square
+            game.pl
           )}
         />
-      {/each}
-      <Circle
-        shape={Object.assign(
-          {
-            color: {
-              idle: "#ff0",
-              alive: "#0f0",
-              dead: "#f00",
-              victory: "#fff",
-            }[game.pl.status],
-          },
-          game.pl
-        )}
-      />
-    </div>
-  </div>
-  {#if ["alive", "idle"].includes(game.pl.status)}
-    <div class="buttons bg-red flex flex-row">
-      <button
-        on:pointerdown={() => (game.pl.movex = +1)}
-        on:pointerup={() => (game.pl.movex = 0)}
-      >
-        →
-      </button>
-      <button
-        on:pointerdown={() => (game.pl.movex = -1)}
-        on:pointerup={() => (game.pl.movex = 0)}
-      >
-        ←
-      </button>
-      <button
-        on:pointerdown={() => (game.pl.movey = +1)}
-        on:pointerup={() => (game.pl.movey = 0)}
-      >
-        ↑
-      </button>
-      <button
-        on:pointerdown={() => (game.pl.movey = -1)}
-        on:pointerup={() => (game.pl.movey = 0)}
-      >
-        ↓
-      </button>
-      <div>
-        {#if game.pl.start}
-          {((new Date().getTime() - game.pl.start) / 1000).toFixed(1)}s
-        {/if}
       </div>
     </div>
-  {:else if death_elapesed}
-    <div class="buttons bg-red">
-      <button class="r" on:click={handleRestart}> restart </button>
-    </div>
-  {/if}
+  </div>
+  <div class="h-20 py-3 flex flex-col items-center">
+    {#if ["alive", "idle"].includes(game.pl.status)}
+      <div class="buttons flex flex-row gap-1">
+        <button
+          on:mousedown={() => (game.pl.movex = +1)}
+          on:touchstart={() => (game.pl.movex = +1)}
+          on:mouseup={() => (game.pl.movex = 0)}
+          on:touchend={() => (game.pl.movex = 0)}
+        >
+          →
+        </button>
+        <button
+          on:mousedown={() => (game.pl.movex = -1)}
+          on:touchstart={() => (game.pl.movex = -1)}
+          on:mouseup={() => (game.pl.movex = 0)}
+          on:touchend={() => (game.pl.movex = 0)}
+        >
+          ←
+        </button>
+        <button
+          on:mousedown={() => (game.pl.movey = +1)}
+          on:touchstart={() => (game.pl.movey = +1)}
+          on:mouseup={() => (game.pl.movey = 0)}
+          on:touchend={() => (game.pl.movey = 0)}
+        >
+          ↑
+        </button>
+        <button
+          on:mousedown={() => (game.pl.movey = -1)}
+          on:touchstart={() => (game.pl.movey = -1)}
+          on:mouseup={() => (game.pl.movey = 0)}
+          on:touchend={() => (game.pl.movey = 0)}
+        >
+          ↓
+        </button>
+      </div>
+    {:else if show_restart}{/if}
+  </div>
 </div>
---
+++ ui/src/lib/Graphics/Text.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import type { Text } from "../../../lib/game";
+
+  export let shape: Text;
+</script>
+
+<div
+  class="absolute rounded-full"
+  style="
+    color: {shape.color ?? '#fff'};
+    text-align: {shape.align};
+    left: {shape.x - shape.w / 2}px;
+    top: {shape.y}px;
+    width: {shape.w}px;
+    "
+>
+  {shape.text}
+</div>
